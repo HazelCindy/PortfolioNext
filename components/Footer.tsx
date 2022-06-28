efe9c100d9bc7f417a47c60861f3b03d1f427bf3
HazelCindy <akinyiagnes.97@gmail.com>
2022-06-28 15:52:42 +0300

    ft:add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+import { Flex, Image, Text } from "@chakra-ui/react";
+import Hazel from "../public/assets/hazel.jpg";
+
+export default function Footer() {
+  return (
+    <Flex flexDir="column" justify="center" align="center" mt="5rem"></Flex>
+  );
+}
